31bb365a72d4affd1d0e03bf064a248bea086448
ZAYA <g.lkhagvazaya@gmail.com>
2022-10-23 15:08:51 -0500

    Add files via upload

---
+++ DinnerDecider/app/src/main/java/com/miu/dinnerdecider/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.miu.dinnerdecider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var inputText:EditText
+    private lateinit var decision:TextView
+    var foods = Foods()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        inputText = findViewById<EditText>(R.id.input_add)
+        decision = findViewById<TextView>(R.id.food_text)
+    }
+
+    fun addFood(view: View) {
+        val text = inputText.text.toString()
+        var alertText = "Added $text"
+        if (text.isNotEmpty()) {
+            foods.addFood(text)
+            inputText.text.clear()
+        } else {
+            alertText = "Please insert your Food name"
+        }
+
+        val toast = Toast.makeText(applicationContext, alertText, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    fun decideFood(view: View) {
+        decision.text = foods.getRandomFood()
+    }
+}
